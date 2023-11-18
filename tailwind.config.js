b250fe189624b9c94e5eb76a27dfae7307aa9e0d
Alyostein <osterrachefurungarn@gmail.com>
2023-11-18 23:22:22 +0700

    Second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    options: {
+      safelist: [
+        "bg-custom-red",
+        "bg-custom-gray",
+        // Add any other custom color classes here
+      ],
+    },
+  },
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-gray": "#151417",
+        "custom-red": "#BC384A",
+      },
+    },
   },
   plugins: [],
 };
